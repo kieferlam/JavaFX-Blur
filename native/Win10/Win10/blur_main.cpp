5f6eeb83c66446013369601bf084df1096b49f17
Kiefer Lam <kieferlam@gmail.com>
2018-12-21 16:15:47 +0000

    Win10 native source

---
+++ native/Win10/Win10/blur_main.cpp
@@ -0,0 +1,86 @@
+#include <jni.h>
+#include <Windows.h>
+#include <string>
+#include "com_kieferlam_javafxblur_NativeBlur.h"
+
+std::string target = "";
+
+void SetWindowBlur(HWND hWnd)
+{
+	const HINSTANCE hModule = LoadLibrary(TEXT("user32.dll"));
+	if (hModule)
+	{
+		struct ACCENTPOLICY
+		{
+			int nAccentState;
+			int nFlags;
+			int nColor;
+			int nAnimationId;
+		};
+		struct WINCOMPATTRDATA
+		{
+			int nAttribute;
+			PVOID pData;
+			ULONG ulDataSize;
+		};
+		typedef BOOL(WINAPI*pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA*);
+		const pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
+		if (SetWindowCompositionAttribute)
+		{
+			ACCENTPOLICY policy = { 3, 0, 0xFF66FFFF, 0 }; // ACCENT_ENABLE_BLURBEHIND=3...
+			WINCOMPATTRDATA data = { 19, &policy, sizeof(ACCENTPOLICY) }; // WCA_ACCENT_POLICY=19
+			SetWindowCompositionAttribute(hWnd, &data);
+
+
+		}
+		FreeLibrary(hModule);
+	}
+}
+void enableBlur(HWND hWnd) {
+	SetWindowBlur(hWnd);
+}
+
+BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam)
+{
+	char String[255];
+
+	if (!hWnd)
+		return TRUE;		// Not a window
+	if (!IsWindowVisible(hWnd))
+		return TRUE;		// Not visible
+	if (!SendMessage(hWnd, WM_GETTEXT, sizeof(String), (LPARAM)String))
+		return TRUE;		// No window title
+
+	char pszClassName[64];
+	GetClassName(hWnd, pszClassName, 64);
+	if (_stricmp(pszClassName, "shell_traywnd") && _stricmp(pszClassName, "progman")) {
+		char windowTitle[64];
+		GetWindowText(hWnd, windowTitle, 64);
+		if (!target.compare(windowTitle)) {
+			enableBlur(hWnd);
+		}
+	}
+
+	return 1;
+}
+
+
+JNIEXPORT void JNICALL Java_com_kieferlam_javafxblur_NativeBlur__1extApplyBlur
+(JNIEnv *env, jobject, jstring title) {
+
+	const jchar *nativeString = env->GetStringChars(title, 0);
+
+	char chars[256];
+
+	for (int i = 0; i < 256; ++i) {
+		const jchar c = nativeString[i];
+		if (c == 0) break;
+		chars[i] = c;
+	}
+
+	std::string string(chars);
+
+	target = string;
+
+	EnumWindows(EnumWindowsProc, 0);
+}
